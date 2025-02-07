4c970a6bc7545abee426435170774a1c1d99c7e6
romanorlovskiy <r.orlovsky@sports.ru>
2025-02-07 19:34:23 +0300

    Add scss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,3 +3,15 @@
 <template>
   <main></main>
 </template>
+
+<style lang="scss">
+/* ваш SCSS код */
+body {
+  background-color: #f0f0f0;
+
+  .container {
+    padding: 20px;
+    color: #333;
+  }
+}
+</style>
