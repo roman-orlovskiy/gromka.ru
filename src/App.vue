07a4b2beac6780b207f314ea1fdcd434aa7efa23
romanorlovskiy <r.orlovsky@sports.ru>
2025-02-07 20:00:12 +0300

    Fix styles

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <div class="app">
+    <RouterView />
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+.app {
+  min-height: 100%;
+  background-color: $color-primary-light;
+}
+</style>
