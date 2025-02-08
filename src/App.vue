6177375355a427e98e51c0e5bcbb89739aaa29bc
romanorlovskiy <r.orlovsky@sports.ru>
2025-02-08 11:23:08 +0300

    Add video

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import { RouterView } from 'vue-router'
 
 <style lang="scss">
 .app {
+  height: 100%;
   min-height: 100%;
   background-color: $color-primary-light;
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,43 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <div class="home">
+    <div class="home__card">
+      <video
+        class="home__video"
+        autoplay
+        loop
+        :muted="!isSoundEnabled"
+        src="/video/gromka.mp4"
+        type="video/mp4"
+      >
+        Ваш браузер не поддерживает видео.
+      </video>
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
-/* ваш SCSS код */
-body {
-  background-color: #f0f0f0;
+<script setup></script>
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
 
-  .container {
-    padding: 20px;
-    color: #333;
+  &__card {
+    width: 32rem;
+    height: 18rem;
+    background-color: $color-white;
+    border-radius: 1rem;
+    border: 1px solid $color-secondary;
+    box-shadow: 0 0 1rem 0 rgba($color-black, 0.5);
+    overflow: hidden;
+  }
+
+  &__video {
+    width: 100%;
+    height: 100%;
+    border-radius: 1rem;
   }
 }
 </style>
