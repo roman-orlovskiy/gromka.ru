64d35e2ff380261f1643161026f6ead8fa4723af
romanorlovskiy <r.orlovsky@sports.ru>
2025-02-19 12:33:42 +0300

    Demo page

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -1,7 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="demo">
+    <div class="demo__editor">
+      <div class="demo__editor-item"></div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+.demo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100%;
+
+  &__editor {
+    display: flex;
+  }
+
+  &__editor-item {
+    width: 2rem;
+    height: 3.5rem;
+    background-color: $color-white;
+    border-radius: 0.2rem;
+    border: 1px solid $color-gray-300;
+  }
+}
+</style>
