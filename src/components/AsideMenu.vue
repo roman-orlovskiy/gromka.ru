8ee4a202536c2dc430f41c11131c7034d10f8620
romanorlovskiy <r.orlovsky@sports.ru>
2025-03-02 17:12:25 +0300

    Add aside component

--- src/components/AsideMenu.vue
+++ src/components/AsideMenu.vue
@@ -12,16 +12,16 @@
 .aside-menu {
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 1rem;
   position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
-  width: 200px;
+  width: 20rem;
   background-color: $color-white;
   z-index: 1000;
-  border-radius: 10px;
-  padding: 10px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 1.5rem;
+  padding: 1.5rem;
+  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
 }
 </style>
