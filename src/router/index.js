bc8d15cde81b95bc80ae6a49d5fae75ad2771f0a
romanorlovskiy <r.orlovsky@sports.ru>
2025-03-31 07:16:51 +0300

    Add pary nn page

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/DemoView.vue'),
     },
+    {
+      path: '/pary-nn',
+      name: 'pary-nn',
+      component: () => import('../views/ParyView.vue'),
+    },
   ],
 })
 
---
+++ src/views/ParyView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="pary">Пари НН</div>
+</template>
+
+<script setup>
+// Здесь будет логика компонента
+</script>
+
+<style scoped lang="scss">
+.pary {
+}
+</style>
