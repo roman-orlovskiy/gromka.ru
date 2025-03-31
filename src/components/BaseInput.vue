8bc3e1fe9d195996e495c367f6ae1fed439d3c14
romanorlovskiy <r.orlovsky@sports.ru>
2025-03-31 07:25:15 +0300

    Add input

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <input
+    type="text"
+    class="input-item"
+    :placeholder="placeholder"
+    :value="value"
+    @input="handleInput"
+  />
+</template>
+
+<script setup>
+defineProps({
+  placeholder: {
+    type: String,
+    default: '',
+  },
+  value: {
+    type: String,
+    default: '',
+  },
+})
+
+const emit = defineEmits(['handleInput'])
+
+const handleInput = (event) => {
+  emit('handleInput', event)
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/variables.scss';
+
+.input-item {
+  width: 100%;
+  border: 0.2rem solid $color-primary;
+  border-radius: 1.5rem;
+  font-size: 2.8rem;
+  padding: 2.5rem 3.5rem;
+  outline: none;
+  color: $color-primary;
+  font-family: $font-default;
+  font-weight: $font-weight-medium;
+  box-sizing: border-box;
+  background-color: $color-white;
+
+  &::placeholder {
+    color: $color-primary;
+    opacity: 0.5;
+  }
+
+  @include layout-mobile {
+    border: 0.2rem solid $color-primary;
+    border-radius: 1.5rem;
+    font-size: 2.8rem;
+    padding: 2.5rem 3.5rem;
+  }
+}
+</style>
--- src/views/ParyView.vue
+++ src/views/ParyView.vue
@@ -1,12 +1,30 @@
 <template>
-  <div class="pary">Пари НН</div>
+  <div class="pary">
+    <h1>Пари НН</h1>
+    <BaseInput placeholder="Введите текст" :value="inputValue" @handleInput="handleInput" />
+  </div>
 </template>
 
 <script setup>
-// Здесь будет логика компонента
+import BaseInput from '@/components/BaseInput.vue'
+import { ref } from 'vue'
+
+const inputValue = ref('')
+
+const handleInput = (event) => {
+  inputValue.value = event.target.value
+}
 </script>
 
 <style scoped lang="scss">
 .pary {
+  padding: 2rem;
+
+  h1 {
+    color: $color-primary;
+    font-size: 3.2rem;
+    margin-bottom: 2rem;
+    font-weight: $font-weight-bold;
+  }
 }
 </style>
