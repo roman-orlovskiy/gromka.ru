75ebf1c490f1056e9ba738666ec358050bfba2b2
romanorlovskiy <r.orlovsky@sports.ru>
2025-03-31 12:56:47 +0300

    Fix

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -44,7 +44,7 @@ const handleChange = (event) => {
   box-sizing: border-box;
   background-color: $color-white;
   appearance: none;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='%23000' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'%3E%3Cpolyline points='6 9 12 15 18 9'%3E%3C/polyline%3E%3C/svg%3E");
+  background-image: $icon-chevron-down;
   background-repeat: no-repeat;
   background-position: right 1rem center;
   background-size: 2rem;
