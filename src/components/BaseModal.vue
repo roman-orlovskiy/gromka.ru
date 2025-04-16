fc903a96312e21618d9a77b85d4ea877eaf416b5
Roman Orlovskiy <irom-io@ya.ru>
2025-04-16 11:44:10 +0300

    Fix auth

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,8 +1,21 @@
 <template>
   <transition name="fade">
     <div class="base-modal" v-if="modalType">
-      <div class="modal-content">
-        <slot></slot>
+      <div class="base-modal__content">
+        <div
+          v-if="modalType === 'login'"
+          class="base-modal__content-inner"
+        >
+          <h2 class="base-modal__title">Авторизация</h2>
+          <p class="base-modal__description">
+            Для авторизации перейдите в телеграм-приложение
+            <a :href="link" target="_blank">@gromkaBot/events</a>
+          </p>
+
+          <a :href="link" target="_blank">
+            <BaseButton>Войти</BaseButton>
+          </a>
+        </div>
       </div>
     </div>
   </transition>
@@ -11,12 +24,14 @@
 <script setup>
 import { useMainStore } from '@/stores/main';
 import { storeToRefs } from 'pinia';
-
+import BaseButton from '@/components/BaseButton.vue';
 const mainStore = useMainStore();
 const { modalType } = storeToRefs(mainStore);
+
+const link = 'https://t.me/gromkaDevBot/events?startapp=auth';
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .base-modal {
   position: fixed;
   top: 0;
@@ -28,12 +43,39 @@ const { modalType } = storeToRefs(mainStore);
   justify-content: center;
   align-items: center;
   z-index: 1500;
-}
 
-.modal-content {
-  background-color: white;
-  padding: 4rem;
-  border-radius: 0.5rem;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  &__content {
+    background-color: white;
+    padding: 4rem;
+    border-radius: 0.5rem;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    width: 100%;
+    max-width: 40rem;
+
+    a {
+      color: $color-primary;
+      text-decoration: underline;
+    }
+  }
+
+  &__content-inner {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1.6rem;
+  }
+
+  &__title {
+    font-size: 3rem;
+    font-weight: 400;
+    text-align: center;
+  }
+
+  &__description {
+    font-size: 1.9rem;
+    font-weight: 400;
+    text-align: center;
+  }
 }
 </style>
