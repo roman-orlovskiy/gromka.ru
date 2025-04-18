bfadbcb1bbe998fcebc7ae55578989ac52950bdf
Roman Orlovskiy <irom-io@ya.ru>
2025-04-19 01:03:03 +0300

    Fix cors

--- public/api/utils/cors.php
+++ public/api/utils/cors.php
@@ -1,11 +1,6 @@
 <?php
 
 function setupCORS() {
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    header("Access-Control-Allow-Credentials: true");
-
     // Обработка preflight-запросов
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(204);
--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? 'https://gromka.ru/api' : '/api',
+  withCredentials: true,
 })
 
 export const getEvents = async () => {
