b5144c423f8517a8d782fafbe07a2217c8006e82
Roman Orlovskiy <irom-io@ya.ru>
2025-04-18 16:11:39 +0300

    Add auth page

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="auth-view">
     <h1 class="auth-view__title">Авторизация</h1>
+    <TelegramAuth />
   </div>
 </template>
 
 <script setup>
+import TelegramAuth from '@/components/TelegramAuth.vue'
 </script>
 
 <style scoped lang="scss">
@@ -12,6 +14,13 @@
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 2rem;
   min-height: 100%;
+  flex-direction: column;
+
+  &__title {
+    font-size: 5rem;
+    font-weight: 700;
+  }
 }
 </style>
