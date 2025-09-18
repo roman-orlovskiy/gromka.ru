629ea09ee84601ee007bbed5098b01a1365416e5
Roman Orlovskiy <irom-io@ya.ru>
2025-09-18 16:57:28 +0300

    Удален компонент PerformanceView и добавлен редирект на '/spartak' в маршрутизаторе.

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 // import HomeView from '@/views/HomeView.vue'
-import PerformanceView from '@/views/PerformanceView.vue'
+// import PerformanceView from '@/views/PerformanceView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: PerformanceView,
+      redirect: '/spartak',
     },
     {
       path: '/spartak',
