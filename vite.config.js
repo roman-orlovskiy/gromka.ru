5b80376b9754a5ea775e4e8dc89d9ef0c029c9d6
Roman Orlovskiy <irom-io@ya.ru>
2025-09-21 14:06:06 +0300

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
+    basicSsl(),
   ],
   resolve: {
     alias: {
@@ -25,6 +27,7 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
+    https: true, // Плагин basicSsl автоматически создаст сертификаты
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
