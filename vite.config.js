6a671b4230b6e2607463be81c9b212725ea7aa06
Roman Orlovskiy <irom-io@ya.ru>
2025-10-23 20:37:38 +0300

    Tunnel

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
     host: true,
     https: false,
     allowedHosts: [
-      'c3hfsf-2a05-fc1-40-3a--c.ru.tuna.am'
+      'heyw3o-2a05-fc1-40-3a--c.ru.tuna.am'
     ],
     proxy: {
       '/api': {
